fed53807c6fad3819293f7a073d81e829b544c50
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-05-26 17:43:26 -0500

    Remove dummy data, refactor JS.

--- main.js
+++ main.js
@@ -3,26 +3,27 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 const searchInput = document.querySelector('.search');
 const suggestionsList = document.querySelector('.suggestions');
 
-let cityData;
 fetch(endpoint)
 	.then(response => response.json())
-	.then(data => {
-		cityData = data;
+	.then(cityData => {
+		searchInput.addEventListener('input', () => filterCityData(cityData));
 	});
 
-searchInput.addEventListener('input', function() {
-	if (!this.value) {
+function filterCityData(cityData) {
+	if (!searchInput.value) {
 		suggestionsList.innerHTML = '<li>Filter for a city</li><li>or a state</li>';
 		return;
 	}
-	const regex = new RegExp(this.value, 'i');
-	const matches = cityData.filter(data => regex.test(data.city) || regex.test(data.state));
-	suggestionsList.innerHTML = '';
-	matchedElements = '';
-	let locationString;
-	matches.forEach(match => {
-		locationString = `${match.city}, ${match.state}`.replace(regex, m => `<span class="hl">${m}</span>`);
-		matchedElements += `<li> <span>${locationString}</span> <span class="population">${match.population}</span></li>\n`;
-	});
-	suggestionsList.innerHTML += matchedElements;
-});
+	const regex = new RegExp(searchInput.value, 'gi');
+	suggestionsList.innerHTML = cityData
+		.filter(data => regex.test(data.city) || regex.test(data.state))
+		.reduce((acc, match) => {
+			const locationString = `${match.city}, ${match.state}`
+				.replace(regex, m => `<span class="hl">${m}</span>`);
+			return acc += 
+				`<li>
+					<span>${locationString}</span> 
+					<span class="population">${match.population}</span>
+				</li>\n`;
+	}, '');
+}
